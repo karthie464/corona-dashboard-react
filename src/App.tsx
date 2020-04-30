831726a62c95a38b82bfe3f79eb72dfaa53e261f
09225363 <meenakshisundaram.subramanyadorai.contractor@pepsico.com>
2020-04-30 20:31:00 +0530

    Title change & Change in API to fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,32 @@ class App extends Component {
   state = {
     states: []
   }
+  overallDetails: any;
+  stateDetails: any;
+  districtDetails: any;
 
 componentDidMount() {
   // fetch('https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise')
-  fetch('https://corona-virus-world-and-india-data.p.rapidapi.com/api_india', 
-      {
+  fetch('https://api.covid19india.org/data.json', 
+      /* {
         headers: {
           'x-rapidapi-host' : 'corona-virus-world-and-india-data.p.rapidapi.com',
           'x-rapidapi-key' : 'eee03be7famsh0a4c97bce963450p17deacjsnf3979d5d6296'
         }
-      })
+      } */)
   .then(res => res.json())
   .then((data) => {
     console.log(data);
-    const finalData = this.dataFormatterForTreeTable(Object.values(data.state_wise));
-    this.setState({ states: finalData.root })
+    this.stateDetails = Object.values(data.statewise);
+/*     const finalData = this.dataFormatterForTreeTable(Object.values(data.statewise));
+    this.setState({ states: finalData.root }) */
+    fetch('https://api.covid19india.org/state_district_wise.json')
+    .then( res => res.json())
+    .then((data) => {
+      this.districtDetails = Object.values(data);
+    const finalData = this.dataFormatterForTreeTable(this.stateDetails, this.districtDetails);
+    this.setState({ states: finalData.root }) 
+    })
   })
   .catch(console.log)
 // this.getTreeTableNodes().then((data: any) => this.setState({states: data}));;
@@ -36,25 +47,27 @@ componentDidMount() {
           .then((res: any) => res.data.root);
 } */
 
-dataFormatterForTreeTable(stateDetails: any) {
+dataFormatterForTreeTable(stateDetails: any, districtDetails: any) {
 
 let stateCount = 0;
 let stateArr = [{}];
 stateDetails.forEach((stateObj: any) => {
   let districtCount = 0;
-  let districtArr = [{}];
-if (stateObj.district !== undefined) {  
-  Object.entries(stateObj.district).forEach((value: any) => {
-    const districtObj = new districtModel(value[0], value[1].active, value[1].confirmed, value[1].recovered, value[1].deceased,
-      value[1].delta.confirmed, value[1].delta.recovered, value[1].delta.deceased);
-    const tempObj = {
-      'key' : stateCount+'-'+districtCount,
-      'data' : districtObj
-    }
-    districtArr.push(tempObj);
-    districtCount++;
-  })
-}
+  let districtArr = [{}];  
+  districtDetails.forEach((value: any) => {
+    if (value.statecode === stateObj.statecode) {
+    Object.entries(value.districtData).forEach((districtDtls: any) => {
+      const districtObj = new districtModel(districtDtls[0], districtDtls[1].active, districtDtls[1].confirmed, districtDtls[1].recovered, districtDtls[1].deceased,
+        districtDtls[1].delta.confirmed, districtDtls[1].delta.recovered, districtDtls[1].delta.deceased);
+      const tempObj = {
+        'key' : stateCount+'-'+districtCount,
+        'data' : districtObj
+      }
+      districtArr.push(tempObj);
+      districtCount++;
+    })
+  }
+})
 const stateData = new stateModel(stateObj.state, stateObj.active, stateObj.confirmed, stateObj.recovered, stateObj.deaths,
             stateObj.deltaconfirmed, stateObj.deltarecovered, stateObj.deltadeaths);
 districtArr.splice(0,1);
@@ -70,7 +83,8 @@ stateArr.splice(0,1);
 const finalData = {
   'root' : stateArr
 };
-
+this.overallDetails = finalData.root.shift();
+finalData.root.push(this.overallDetails);
 return finalData;
 
 }
